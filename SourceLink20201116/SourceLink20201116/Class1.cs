0f69185a93e1dd72971d643f2a8467df3be00e15
Li Shuai <lishuai@diyibox.com>
2020-11-16 19:31:45 +0800

    commit

---
+++ SourceLink20201116/SourceLink20201116/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SourceLink20201116
+{
+    public class SourceLink20201116
+    {
+        public void Console()
+        { 
+            System.Console.WriteLine("SourceLink20201116");
+        
+        }
+
+    }
+}
